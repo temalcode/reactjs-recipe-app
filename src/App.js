308d24846f0687f25589f19259f574b48619b49a
Temal Code <79495831+temalcode@users.noreply.github.com>
2022-08-09 05:50:45 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -11,6 +11,9 @@ function App() {
         <Category/>
         <Pages/>
       </BrowserRouter>
+      <div style={{marginTop: "100px", marginBottom: "50px"}}>
+        <p>Some icons are from <a href="https://fontawesome.com/">Font Awsome - version 5.15.4</a> under <a href="https://creativecommons.org/licenses/by/4.0/">CC BY 4.0 License</a>.</p>
+      </div>
     </div>
   );
 }
